cdada82a7aece5b659036490e8679a50e80ec9a5
Martin Gleich <martin.gleich2@googlemail.com>
2024-12-19 12:54:48 +0100

    Solved next problem and added two helpers

--- Excercise02.lean
+++ Excercise02.lean
@@ -24,16 +24,33 @@ example : (∃ x, p x ∨ q x) ↔ (∃ x, p x) ∨ (∃ x, q x) := Iff.intro
    (fun hp : ∃ x, p x => match hp with | ⟨w, hw⟩ => Exists.intro w (Or.inl hw))
     fun hq : ∃ x, q x => match hq with | ⟨w, hw⟩ => Exists.intro w (Or.inr hw))
 
+theorem A : (y : α) → (∀ x : α, p x) → (¬ ∀ x, ¬ p x) := fun (x) (all_x) (all_not_x) => have p_x := all_x x
+  have not_p_x := all_not_x x
+  absurd p_x not_p_x
+
 example : (∀ x, p x) ↔ ¬ (∃ x, ¬ p x) := Iff.intro
  (fun (all_p_w : ∀ (x : α), p x) (exists_not_p_w : ∃ x, ¬p x) => match exists_not_p_w with
  | ⟨w, not_p_w⟩ => have p_w : p w := all_p_w w
   absurd p_w not_p_w)
- (fun (not_h : ¬ ∃ x, ¬p x) (a : α) => Or.elim (em (p a))
-  id
+ (fun (not_h : ¬ ∃ x, ¬p x) (a : α) => show p a from byContradiction
   (fun hnpa : ¬ p a => have h : ∃ x, ¬ p x := Exists.intro a hnpa
     absurd h not_h))
 
-example : (∃ x, p x) ↔ ¬ (∀ x, ¬ p x) := sorry
+theorem by_contrapositive {P Q : Prop}: (¬ Q → P) → ¬ P → Q := (fun h => fun hnp => byContradiction
+  (fun hnq => have p : P := h hnq
+    absurd p hnp))
+
+example : (∃ x, p x) ↔ ¬ (∀ x, ¬ p x) := Iff.intro
+  -- Forward direction
+  (fun (exists_x_with_property_p) (all_x_have_property_not_p) => match exists_x_with_property_p with
+    | ⟨x, proof_of_p_x⟩ => have proof_of_not_p_x := all_x_have_property_not_p x
+      absurd proof_of_p_x proof_of_not_p_x
+  )
+  -- Backward direction
+  ( -- Proof the contrapositive
+    by_contrapositive (show ¬ (∃ x, p x) → (∀ x, ¬ p x) from fun (ne) (x) (px) => ne ⟨x, px⟩)
+  )
+
 example : (¬ ∃ x, p x) ↔ (∀ x, ¬ p x) := sorry
 example : (¬ ∀ x, p x) ↔ (∃ x, ¬ p x) := sorry
 
